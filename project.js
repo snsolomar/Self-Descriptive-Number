2128fbfe237739ea1ba974b6f4e5aa1a36e8dff7
Lordprettyflacko <snsolomar@gmail.com>
2023-01-03 11:45:29 -0600

    second commit

--- project.js
+++ project.js
@@ -10,10 +10,10 @@ function isSelfDescribing(num) {
     .map((str) => Number(str));
 
   let i = 0;
-  for (const num of numArray) {
+  for (const value of numArray) {
     //check to see how times times the current index appears in the numArray
     //does that total match the current element/number for this iteration
-    if (numArray.filter((n) => n === i).length === num) {
+    if (numArray.filter((n) => n === i).length === value) {
       //incremenet i to get to the next interation of the loops
       i++;
       continue;
